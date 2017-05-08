4ca15b7273d8bd6d82f500e57fa37c91e72f878d
StMark <prizrack91@gmail.com>
2017-05-09 01:48:21 +0700

    Создание анимации героя при перемещении в стороны

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(){
 	
 	Texture t;
 	t.loadFromFile("fang.png");
-
+	float currentFrame = 0;
 	Sprite s;
 	s.setTexture(t);
 	s.setTextureRect(IntRect(0,244,40,50));
@@ -19,8 +19,18 @@ int main(){
 			if (event.type == Event::Closed)
 				game.close();
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Left)) s.move(-0.1,0);
-		if (Keyboard::isKeyPressed(Keyboard::Right)) s.move(0.1,0);
+		if (Keyboard::isKeyPressed(Keyboard::Left)){
+			s.move(-0.05,0);
+			currentFrame += 0.003;
+	  		if (currentFrame > 6) currentFrame -=6 ;
+			s.setTextureRect(IntRect(40*int(currentFrame)+40,244,-40,50));
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Right)){
+			s.move(0.05,0);
+			currentFrame += 0.003;
+	  		if (currentFrame > 6) currentFrame -=6 ;
+			s.setTextureRect(IntRect(40*int(currentFrame),244,40,50));
+		}
 		if (Keyboard::isKeyPressed(Keyboard::Up)) s.move(0,-0.1);
 		if (Keyboard::isKeyPressed(Keyboard::Down)) s.move(0,0.1);
 		game.clear(Color::White);
