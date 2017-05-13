7159489fe5d67c8c4915d98ed2ba9c819d8a8955
MariaNosova111 <mariykanosova1998@yandex.ru>
2017-05-13 15:04:41 +0700

    Создание гравитации

--- src/main.cpp
+++ src/main.cpp
@@ -2,39 +2,70 @@
 
 using namespace sf;
 
+int ground = 150;
+
+class PLAYER {
+
+public:
+
+float dx,dy;
+FloatRect rect;
+bool onGround;
+Sprite sprite;
+float currentFrame;
+
+PLAYER(Texture &image){
+	sprite.setTexture(image);
+	rect = FloatRect(7*32,9*32,40,50);
+	dx=dy=0.1;
+	currentFrame = 0;
+   }
+void update(float time){	
+	rect.left += dx * time;
+	if (!onGround) dy=dy+0.0005*time;
+	rect.top += dy*time;
+	onGround=false;
+	currentFrame += 0.005*time;
+	if (currentFrame > 6) currentFrame -=6 ;
+	if (dx>0) sprite.setTextureRect(IntRect(40*int(currentFrame),244,40,50));
+	if (dx<0) sprite.setTextureRect(IntRect(40*int(currentFrame)+40,244,-40,50));
+	if (rect.top > ground){
+		rect.top = ground;
+		dy=0;
+		onGround = true;
+	}
+	sprite.setPosition(rect.left, rect.top);
+	dx=0;
+   }
+};
+
 int main(){
 	RenderWindow game(VideoMode(600, 400), "Game");
 	
 	Texture t;
 	t.loadFromFile("fang.png");
 	float currentFrame = 0;
-	Sprite s;
-	s.setTexture(t);
-	s.setTextureRect(IntRect(0,244,40,50));
-	s.setPosition(50,100);
-
+	PLAYER p(t);
+	Clock clock;
 	while (game.isOpen()){
+		float time = clock.getElapsedTime().asMicroseconds();
+		clock.restart();
+		time = time/700;
 		Event event;
 		while(game.pollEvent(event)){
 			if (event.type == Event::Closed)
 				game.close();
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Left)){
-			s.move(-0.05,0);
-			currentFrame += 0.003;
-	  		if (currentFrame > 6) currentFrame -=6 ;
-			s.setTextureRect(IntRect(40*int(currentFrame)+40,244,-40,50));
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Right)){
-			s.move(0.05,0);
-			currentFrame += 0.003;
-	  		if (currentFrame > 6) currentFrame -=6 ;
-			s.setTextureRect(IntRect(40*int(currentFrame),244,40,50));
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Up)) s.move(0,-0.1);
-		if (Keyboard::isKeyPressed(Keyboard::Down)) s.move(0,0.1);
+		if (Keyboard::isKeyPressed(Keyboard::Left))p.dx = -0.1;
+		if (Keyboard::isKeyPressed(Keyboard::Right))p.dx = 0.1;
+		if (Keyboard::isKeyPressed(Keyboard::Up)) 
+		if (p.onGround){
+				p.dy=-0.35;
+				p.onGround=false;
+			}
+		p.update(time);
 		game.clear(Color::White);
-		game.draw(s);
+		game.draw(p.sprite);
 		game.display();
 	}
 	return 0;
