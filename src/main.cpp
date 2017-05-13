408ed782ddb55251f13811f3467add66abf1c9e1
StMark <prizrack91@gmail.com>
2017-05-13 18:18:38 +0700

    Взаимодействие с картой и обработка столкновений 2.0, взаимодействие с обьектами

--- src/main.cpp
+++ src/main.cpp
@@ -41,11 +41,11 @@ PLAYER(Texture &image){
 
 void update(float time){	
 	rect.left += dx * time;
-	CollisionX();
+	Collision(0);
 	if (!onGround) dy=dy+0.0005*time;
 	rect.top += dy*time;
 	onGround=false;
-	CollisionY();
+	Collision(1);
 	currentFrame += 0.005*time;
 	if (currentFrame > 6) currentFrame -=6 ;
 	if (dx>0) sprite.setTextureRect(IntRect(40*int(currentFrame),244,40,50));
@@ -59,31 +59,28 @@ void update(float time){
 	dx=0;
    }
 
-void CollisionX(){
+void Collision(int dir){
 	for (int i = rect.top/32 ; i<(rect.top+rect.height)/32; i++)
-	for (int j = rect.left/32; j<(rect.left+rect.width)/32; j++){
-		if (TileMap[i][j]=='B'){ 
-	        	if (dx>0) rect.left =  j*32 - rect.width; 
-			if (dx<0) rect.left =  j*32 + 32;
-		}
-	}
-}
+		for (int j = rect.left/32; j<(rect.left+rect.width)/32; j++){ 
+	  		if (TileMap[i][j]=='B'){ 
+	        		if ((dx>0) && (dir==0)) rect.left =  j*32 -  rect.width; 
+				if ((dx<0) && (dir==0)) rect.left =  j*32 + 32;	
+				if ((dy>0) && (dir==1)){
+					rect.top = i*32 - rect.height;
+					dy=0;
+					onGround=true;
+				}
 
-void CollisionY(){
-	for (int i = rect.top/32 ; i<(rect.top+rect.height)/32; i++)
-	for (int j = rect.left/32; j<(rect.left+rect.width)/32; j++){
-		if (TileMap[i][j]=='B'){ 
-	        	if (dy>0){
-				rect.top =  i*32 - rect.height; 
-				dy=0;
-				onGround = true;
-			}
-			if (dy<0){
-				rect.top =  i*32 + 32;
-				dy=0;
+				if ((dy<0) && (dir==1)){
+					rect.top = i*32 + 32;
+					dy=0;
 				}
 			}
-		}
+
+		 	if (TileMap[i][j]=='0'){ 
+				TileMap[i][j]=' ';
+	                } 	
+  		}
 	}
 };
 int main(){
