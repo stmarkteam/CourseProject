3deda4af85ae2c83242208a035e315e44a82e5c2
StMark <prizrack91@gmail.com>
2017-05-20 23:34:43 +0700

    Добалено условие проигрыша, а так же вывод уведомления о нём, надо доработать

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ float offsetX=0, offsetY=0;
 
 const int H = 30;
 const int W = 133;
+bool life = 0;
 
 String TileMap[H] = {
 "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
@@ -48,10 +49,11 @@ FloatRect rect;
 bool onGround;
 Sprite sprite;
 float currentFrame;
+//int n = 23, l;
 
 PLAYER(Texture &image){
 	sprite.setTexture(image);
-	rect = FloatRect(6*32,8*32,40,50);
+	rect = FloatRect(7*32,9*32,40,50);
 	dx=dy=0.1;
 	currentFrame = 0;
    }
@@ -63,6 +65,7 @@ void update(float time){
 	rect.top += dy*time;
 	onGround=false;
 	Collision(1);
+	//if (l==7) life = false;
 	currentFrame += 0.005*time;
 	if (currentFrame > 6) currentFrame -=6 ;
 	if (dx>0) sprite.setTextureRect(IntRect(40*int(currentFrame),244,40,50));
@@ -71,7 +74,7 @@ void update(float time){
 	dx=0;
    }
 
-void Collision(int dir){
+int Collision(int dir){
 	for (int i = rect.top/32 ; i<(rect.top+rect.height)/32; i++)
 		for (int j = rect.left/32; j<(rect.left+rect.width)/32; j++){ 
 	  		if (TileMap[i][j]=='P' || TileMap[i][j]=='0'|| TileMap[i][j]=='w' || TileMap[i][j]=='n' || TileMap[i][j]=='a') 					{ 
@@ -89,18 +92,26 @@ void Collision(int dir){
 				}
 			}
 
-		 	if (TileMap[i][j]=='r' || TileMap[i][j]=='Z'){ 
+		 	if (TileMap[i][j]=='r'){ 
+				TileMap[i][j]=' ';
+				//n--;
+				//if (n==0) return 7;
+	                } 
+			if (TileMap[i][j]=='Z'){ 
 				TileMap[i][j]=' ';
 	                } 	
 			if (TileMap[i][j]=='q'){ 
-				sprite.setColor(Color::Red);
+				life = false;
 	                } 	
   		}
+	return 0;
 	}
 };
 
 int main(){
 	RenderWindow game(VideoMode(600, 400), "Game");
+	Font font;
+    	font.loadFromFile("sansation.ttf");
 	
 	Texture t, m, f;
 	t.loadFromFile("fang.png");
@@ -110,6 +121,20 @@ int main(){
 	Texture menu1,menu2;
 	menu1.loadFromFile("1.png");
     	menu2.loadFromFile("2.png");
+	
+	Text dead;
+   	dead.setFont(font);
+  	dead.setCharacterSize(50);
+   	dead.setPosition(200.f, 150.f);
+   	dead.setColor(Color::White);
+   	dead.setString("You dead!");
+	
+	Text start;
+    	start.setFont(font);
+    	start.setCharacterSize(20);
+    	start.setPosition(160.f, 300.f);
+    	start.setColor(Color::White);
+    	start.setString("<press space to start the game>");
 
 	Sprite m1(menu1),m2(menu2);
 
@@ -136,7 +161,10 @@ int main(){
 		MenuNum=2;
 	}
 	if (Mouse::isButtonPressed(Mouse::Left)){
-		if (MenuNum==1) Menu=false;
+		if (MenuNum==1){
+			Menu=false;
+			life=true;
+		}
 		if (MenuNum==2) {
 			game.close();
 			Menu=false;
@@ -152,14 +180,18 @@ int main(){
 	PLAYER p(t);
 	Clock clock;
 	while (game.isOpen()){
-		float time = clock.getElapsedTime().asMicroseconds();
-		clock.restart();
-		time = time/700;
 		Event event;
 		while(game.pollEvent(event)){
 			if (event.type == Event::Closed)
 				game.close();
-		}
+			if ((event.type == Event::KeyPressed) && (event.key.code == Keyboard::Space)){
+				life=true;
+				p.rect = FloatRect(7*32,9*32,40,50);
+			}
+		while (life){
+			float time = clock.getElapsedTime().asMicroseconds();
+			clock.restart();
+			time = time/700;
 			if (Keyboard::isKeyPressed(Keyboard::Left))p.dx = -0.1;
 			if (Keyboard::isKeyPressed(Keyboard::Right))p.dx = 0.1;
 			if (Keyboard::isKeyPressed(Keyboard::Up)){ 
@@ -169,25 +201,34 @@ int main(){
 			}
 		}
 		p.update(time);
-
+		
 		if ((p.rect.left>300) && (p.rect.left<3930)) offsetX = p.rect.left - 300;
         	offsetY = p.rect.top - 200;
 
-		game.draw(sBackground);
-		for (int i=0; i<H; i++)
-		for (int j=0; j<W ; j++){ 
-			if (TileMap[i][j]=='0') tile.setTextureRect(IntRect(416,128,32,32));
-			if (TileMap[i][j]=='P') tile.setTextureRect(IntRect(416,128,32,32));
-			if (TileMap[i][j]=='w') tile.setTextureRect(IntRect(416,128,32,32));
-			if (TileMap[i][j]=='r') tile.setTextureRect(IntRect(160,256,32,32));
-			if (TileMap[i][j]=='q') tile.setTextureRect(IntRect(160,128,32,32));
-			if (TileMap[i][j]=='a') tile.setTextureRect(IntRect(160,128,32,32));
-			if (TileMap[i][j]==' ') continue;
-			tile.setPosition(j*32-offsetX,i*32 - offsetY) ;
-		        game.draw(tile);
-	       	 }
-		game.draw(p.sprite);
+		if (life){
+			game.draw(sBackground);
+			for (int i=0; i<H; i++)
+			for (int j=0; j<W ; j++){ 
+				if (TileMap[i][j]=='0') tile.setTextureRect(IntRect(416,128,32,32));
+				if (TileMap[i][j]=='P') tile.setTextureRect(IntRect(416,128,32,32));
+				if (TileMap[i][j]=='w') tile.setTextureRect(IntRect(416,128,32,32));
+				if (TileMap[i][j]=='r') tile.setTextureRect(IntRect(160,256,32,32));
+				if (TileMap[i][j]=='q') tile.setTextureRect(IntRect(160,128,32,32));
+				if (TileMap[i][j]=='a') tile.setTextureRect(IntRect(160,128,32,32));
+				if (TileMap[i][j]==' ') continue;
+				tile.setPosition(j*32-offsetX,i*32 - offsetY) ;
+		        	game.draw(tile);
+	       	 	}
+			game.draw(p.sprite);
+		}
+		else {
+			game.draw(sBackground);
+			game.draw(dead);
+			game.draw(start);
+		}
 		game.display();
+	}
+	}
 	}
 	return 0;
 }
